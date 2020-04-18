c4ee452ba9ba9d8e2c1956ebfb9c3205699eb412
shreya-nipanikar <57978423+shreya-nipanikar@users.noreply.github.com>
2020-04-18 05:49:15 -0400

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,26 @@
+create database CONFERENCE_REVIEW;
+
+USE CONFERENCE_REVIEW;
+
+Create table author
+(EmailAddress  Varchar(60) NOT NULL PRIMARY KEY,
+FirstName  Char(20),
+LastName Char(20));
+
+EXEC sp_columns author;
+
+DROP TABLE dbo.author;
+
+ALTER TABLE author
+ADD PRIMARY KEY(EmailAddress);
+
+Create table Reviewer
+(R_Email  Varchar(60) NOT NULL PRIMARY KEY,
+FirstName Char(20),
+LastName  Char(20),
+PhoneNo   int,
+AuthorFeedback  Varchar(200),
+CommiteeFeedback Varchar(200),
+Affiliation Varchar(30));
+
+EXEC sp_columns Reviewer;
